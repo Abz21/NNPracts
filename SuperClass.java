82589c1e7e4df724ace6b92f97dce4e52433624c
Abz21 <70847776+Abz21@users.noreply.github.com>
2022-01-20 20:19:42 +0530

    Initial Commit

---
+++ SuperClass.java
@@ -0,0 +1,12 @@
+package com.practice.nnpractice.polymorphism;
+
+public class SuperClass extends Object{
+
+	public void method1(int a){
+		System.out.println("I am from super class method1()");
+	}
+	
+	public String toString() {
+		return "I am from toString method of superclass";
+    }
+}
